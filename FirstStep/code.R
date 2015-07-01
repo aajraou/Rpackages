c08762474480a97303a52de67260810b97cc46ab
aajraou <abed.ajraou@gmail.com>
2015-07-01 20:42:49 +0200

    Revert "Add Filter & Modifications"
    This reverts commit 52dba26767b062e785b074c649285f360f22efb7.

--- FirstStep/code.R
+++ FirstStep/code.R
@@ -3,9 +3,7 @@
 36^0.5
 4:2
 (4+1)*(5+3^2-9)/3
-##############
-#Variables
-##############
+#Variable
 x<-10*8
 #Vector
 color<-c("Blue","Red","Black")
@@ -15,7 +13,7 @@ sky.week
 logical<-c(TRUE,FALSE,TRUE)
 logical
 numbers.1<-c(1:15)
-#Matrice
+#Matrix
 matrix.1<-matrix(numbers.1,ncol=3)
 matrix.1
 dim(matrix.1)<-c(5,3)
@@ -32,14 +30,4 @@ dataframe.2<-data.frame(color,sky.week,logical)
 dataframe.2
 #Liste
 liste.1<-list(x,color,matrix.1,dataframe.2)
-##############
-#Filter/Modifications
-##############
-sum(matrix.2)
-apply(matrix.2,1,sum) #by rows
-matrix.2[matrix.2>=95 & matrix.2<=105]
-matrix.2[,-2] #extract the second column
-matrix.2-2
-rbind(matrix.2,c(20,10,0,1,-2))
-dimnames(matrix.2)<-list(c("r1","r2"),c("c1","c2","c3","c4","c5","c6","c7","c8"))
-sky.week[which(sky.week=="Raining")]<-"Raining+"
+
